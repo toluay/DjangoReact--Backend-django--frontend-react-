fff835aab1a2e383e8e1e7e5580c4ad2eea0720f
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-20 14:51:03 +0100

    views.py  added to the todo applicatio

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .serializers import TodoSerializer
+from rest_framework import viewsets
+from .models import Todo
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
