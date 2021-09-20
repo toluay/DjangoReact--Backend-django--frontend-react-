48a8daade708d6c270ff97fb54c208f58510acb8
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-20 14:45:40 +0100

    backend application added and model created

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+# Create your models here.
+from django.db import models
+
 class Todo(models.Model):
    title = models.CharField(max_length=100)
    description = models.TextField()
